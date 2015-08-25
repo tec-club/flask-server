b2554b6bce0d4cb2ce1b7b554abd397d83124fe0
colehertz <chrh02@gmail.com>
2015-08-24 20:12:20 -0700

    initial commit

---
+++ app/endpoints.py
@@ -0,0 +1,32 @@
+from flask.ext.httpauth import HTTPBasicAuth
+from flask import *
+from app import app
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_password(email):
+	if email == 'test@gmail.com':
+		return 'password'
+	return none
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'unauthorized'}), 401)
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return "Hello, world!"
+
+
+#Retreive the next late start date from the current date
+@app.route('/api/late_start/next_date')
+@auth.login_required
+def getNextday():
+	return "Monday August 14"
+
+#Retreive upcoming news or announcements
+@app.route('/api/announcements')
+def getAnnouncements(announcements):
+	return "announcements"
